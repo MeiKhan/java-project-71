55a571216ac37a3d8a8aa0bae875c3b0210f06a2
MeiKhan <meirjan28@mail.ru>
2025-04-02 16:09:10 +0500

    picocli option added

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,36 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff",
+        description = "Compares two configuration files and shows a difference.")
+
+public class App implements Callable<Integer> {
+
+/*    @Parameters(index = "0", description = "The file whose checksum to calculate.", hidden = true)
+    private File file;*/
+
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
+    private boolean helpRequested = false;
+    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
+    private boolean alg;
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
